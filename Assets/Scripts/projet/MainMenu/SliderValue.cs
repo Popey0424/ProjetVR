1bc1da48acf6134cebed3d1e5fdac32b28ed83e3
P3D\Louis.roy <roylouis0424@gmail.com>
2024-02-15 12:22:55 +0100

    Menu fake

---
+++ Assets/Scripts/projet/MainMenu/SliderValue.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class SliderValue : MonoBehaviour
+{
+
+    public TMPro.TextMeshProUGUI TextValue;
+    public Slider Slider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void OnValueChanged(float newValue)
+    {
+        int valueInt = (int)Mathf.Round(newValue * 100.0f);
+        TextValue.text = valueInt.ToString();
+    }
+
+
+
+}
